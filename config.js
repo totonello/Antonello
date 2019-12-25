16871dfd290ca694fc6ad7c5938392254f62c97e
Totonello <totonello.catanese@gmail.com>
2019-12-25 19:49:51 +0100

    3.0

---
+++ config.js
@@ -0,0 +1,2 @@
+exports.TOKEN = "NjU4NzMyMDg3NjExODgzNTQw.XgOttw.ppg-xAsdkhLmoZ6iEH1N7i1LOmg";
+exports.PREFIX = "o!";
--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const { Client } = require("discord.js");
+const { TOKEN, PREFIX } = require("./config");
 const client = new Client({ disableEveryone: true });
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("Je suis ok");
 });
 
 client.on("message", msg => {
-  if (msg.content === "ping") msg.channel.send("Pong!");
-  if (msg.content === "everyone") msg.channel.send("@everyone salut à tous", { disableEveryone: false });
-  if (msg.content === "noteveryone") msg.channel.send("@everyone (noteveryone), salut à tous !");
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send("Pong!");
+  if (msg.content.startsWith(`${PREFIX}pong`)) msg.channel.send("Ping!");
 });
 
-client.login("NjU4NzMyMDg3NjExODgzNTQw.XgOrBQ.t9Y4RO9ReP3rpG0aKQhVFSORmAo");
+client.login(TOKEN);
