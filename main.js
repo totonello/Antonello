054442afdda11c56e56a1d1932eddc9353b82eec
Totonello <totonello.catanese@gmail.com>
2019-12-25 19:37:09 +0100

    2.0

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const { Client } = require('discord.js');
-const client = new Client({disableEveryone : true });
+const { Client } = require("discord.js");
+const client = new Client({ disableEveryone: true });
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') msg.channel.send('Pong!');
-  if (msg.content === 'everyone') msg.channel.send('@everyone salut à tous', { disableEveryone: false });
-  if (msg.content === 'noteveryone') msg.channel.send('@everyone (noteveryone), salut à tous !');
+client.on("message", msg => {
+  if (msg.content === "ping") msg.channel.send("Pong!");
+  if (msg.content === "everyone") msg.channel.send("@everyone salut à tous", { disableEveryone: false });
+  if (msg.content === "noteveryone") msg.channel.send("@everyone (noteveryone), salut à tous !");
 });
 
-client.login('NjU4NzMyMDg3NjExODgzNTQw.XgObig._Sp9sIsCytuj99Mt3oCROilGH3Q');
+client.login("NjU4NzMyMDg3NjExODgzNTQw.XgOrBQ.t9Y4RO9ReP3rpG0aKQhVFSORmAo");
